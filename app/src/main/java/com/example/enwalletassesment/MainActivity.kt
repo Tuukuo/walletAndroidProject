e9329fc254a27e0276576240275505c6aa058abf
student <tuukuodorcas@gmail.com>
2024-07-08 07:42:39 +0300

    first commit

---
+++ app/src/main/java/com/example/enwalletassesment/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.enwalletassesment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class MainActivity : AppCompatActivity() {
+//    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        setContentView(binding.root)
+
+
+    }
+}
+
+
+
+
+
+
+
+//override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    binding=ActivityMainBinding.inflate(layoutInflater)
+//    setContentView(binding.root)
+//    binding.rvConacts.layoutManager= LinearLayoutManager(this)
+//    displayContacts()
+
+
+}
---
+++ app/src/main/java/com/example/enwalletassesment/Wallet.kt
@@ -0,0 +1,8 @@
+package com.example.enwalletassesment
+
+data class Wallet(
+    var name: String,
+    var phoneNumber: String,
+    var email: String,
+    var avatar: String
+)
